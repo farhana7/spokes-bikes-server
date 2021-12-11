e13ec325b16de51e5cf870e04f7c388828f4f4c1
farhana7 <farhanaadf@gmail.com>
2021-12-11 21:15:21 +0600

    d added

--- index.js
+++ index.js
@@ -1,9 +1,61 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
 const port = process.env.PORT || 5000;
 
+//middleware
 app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ir5um.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("spokes_bikes");
+    const productCollection = database.collection("products");
+    // console.log("database connected successfully");
+
+    //GET Products API
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      res.json(products);
+    });
+
+    //Get Single Product
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log("getting specific product", id);
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.json(product);
+    });
+
+    //POST API
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      console.log("hit the post api", product);
+      const result = await productCollection.insertOne(product);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello Spokes Bikes!");
